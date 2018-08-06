00bc1dfaf52771b45295c9cd45c000d45ad10977
zhongshsh <zhongshsh@dogesoft.cn>
2018-08-06 16:30:13 +0800

    rate 改造

--- components/rate/index.js
+++ components/rate/index.js
@@ -1,9 +1,29 @@
 import Rate from "antd/lib/rate/index";
-import React from 'react';
+import React , {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-const JwRate= (props) => (
-    <Rate className="jw-rate" {...props} />
-);
+class JwRate extends Component{
+  componentDidMount(){
+    if(this.props.character=="number"){
+      let jw_rate = $(ReactDOM.findDOMNode(this.refs.jwRate)).find('li');
+      var min = this.props.min;
+      var max = this.props.max;
+      for(var i = min; i<= max; i++){
+        if(min==0){
+          jw_rate.eq(i).find('.ant-rate-star-first')[0].innerHTML = i;
+          jw_rate.eq(i).find('.ant-rate-star-second')[0].innerHTML = i;
+        }else{
+          jw_rate.eq(i-1).find('.ant-rate-star-first')[0].innerHTML = i;
+          jw_rate.eq(i-1).find('.ant-rate-star-second')[0].innerHTML = i;
+        }
+      }
+    }
+  }
+  render(){
+    return(
+      <Rate className={"jw-rate " + (this.props.character=="number" ? 'jw-rate-number' : '')} ref="jwRate" {...this.props} />
+    )
+  }
+}
 
 export default JwRate;
